3637da094b9e95225921396c5fff5df5f542199d
tekinndag <tekindag90@gmail.com>
2022-07-03 02:22:50 +0100

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello JAVA");
+        System.out.println("Devoloping hello feature");
+
+        System.out.println("Local repo is already published to github");
+        System.out.println("Doing more work.");
     }
 }
